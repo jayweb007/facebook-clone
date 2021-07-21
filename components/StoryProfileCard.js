ff5284b05afa6d5842fbda0fde42b5b654994414
jayweb007 <jaymomoh@gmail.com>
2021-07-21 11:38:49 -0400

    commit on main

--- components/StoryProfileCard.js
+++ components/StoryProfileCard.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { PlusCircleIcon } from "@heroicons/react/solid";
 
 function StoryProfileCard({ name, src }) {
   return (
@@ -6,26 +7,32 @@ function StoryProfileCard({ name, src }) {
       className="relative h-52 w-[120px] cursor-pointer  overflow-x p-3
     transition duration-200 ease-in hover:scale-105 hover:animate-pulse"
     >
-      <div className="absolute h-[45px] w-[45px] rounded-full border-4 border-blue-500 z-50 top-3 left-3">
-        <Image
+      <div className="absolute h-[35px] w-[35px] z-50 bottom-[30px] left-[26px] lg:left-[42px] rounded-full border-none border-blue-500 ">
+        {/* <Image
           src={src}
-          width={40}
-          height={40}
+          width={30}
+          height={30}
           objectFit="cover"
           className="rounded-full"
-        />
+        /> */}
+        <PlusCircleIcon className=" h-[35px] w-[35px] border-2 bg-white border-gray-50 rounded-full text-blue-600 " />
       </div>
       <Image
         src={src}
         layout="fill"
         className="object-cover filter brightness-75 rounded-2xl lg:rounded-3xl "
       />
-      <p
+      <div className="absolute z-1 bottom-0 left-0 rounded-bl-2xl rounded-br-2xl h-[50px] w-full bg-white">
+        <p className="ml-[8px] mt-[24px] text-xs break-words lg:text-sm lg:ml-[18px] leading-tight text-black font-semibold">
+          Create Story
+        </p>
+      </div>
+      {/* <p
         className="absolute z-50 top-[150px] left-3 text-sm break-words 
       lg:text-base lg:left-1 leading-tight text-white font-normal"
       >
         {name}
-      </p>
+      </p> */}
     </div>
   );
 }
